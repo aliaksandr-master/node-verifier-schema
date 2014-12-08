c4a6ac4469be31c8538b9593e2963b845bb8732d
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-09 00:18:03 +0300

    add tests for clone, nested schema adding

--- test/main.js
+++ test/main.js
@@ -1,7 +1,13 @@
 "use strict";
 
+var _ = require('lodash');
+
 var inspect = function (value) {
 	return '\n' + require('util').inspect(value, {depth: null, colors: true}) + '\n';
+};
+
+var consoleInspect = function (value) {
+	console.log.apply(console, _.map(arguments, inspect));
 };
 
 var validator = require('node-verifier');
@@ -42,6 +48,69 @@ var validateSchema1 = _schemaBuilder('type object', function (required, optional
 });
 
 var validateSchema2 = _schemaBuilder('type object');
+
+exports.schema = {
+	simple: function (test) {
+		//_schemaBuilder('type object', function (required, optional) {
+		//	required('fio', 'type object', function (required, optional) {
+		//		required('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+		//		required('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+		//		optional('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+		//	});
+		//	required('age', ['type number', 'max_value 100', 'min_value 16']);
+		//	required('family', ['type array', 'min_length 2', {each: ['type object']}], function (required, optional) {
+		//		required('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+		//		required('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+		//		optional('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+		//		optional('age', ['type number', 'max_value 100', 'min_value 16']);
+		//	});
+		//	required('school_names', ['type array', 'min_length 2', 'not empty', {each: ['type string', 'min_length 3', 'max_length 20']}]);
+		//});
+		test.ok(true);
+		test.done();
+	},
+
+	clone: function (test) {
+		var s0 = _schemaBuilder('type object', function (required, optional) {
+			required('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+			required('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+			optional('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+		});
+
+		test.deepEqual(s0.schema, s0.schema.clone());
+		test.done();
+	},
+
+	nestedSchema: function (test) {
+		var s0 = _schemaBuilder(null, function (required, optional) {
+			required('fio', 'type object', function (required, optional) {
+				required('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+				required('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+				optional('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+			});
+		});
+		//consoleInspect(s0.schema);
+
+		var s1 = _schemaBuilder('type object', function (required, optional) {
+			required('age', ['type number', 'max_value 100', 'min_value 16']);
+			required('family', ['type array', 'min_length 2', {each: ['type object']}], function (required, optional) {
+				required('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+				required('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+				optional('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+				optional('age', ['type number', 'max_value 100', 'min_value 16']);
+			});
+			required('school_names', ['type array', 'min_length 2', 'not empty', {each: ['type string', 'min_length 3', 'max_length 20']}]);
+		});
+		//consoleInspect(s1.schema);
+
+		s1.schema.field('fio', false, null, s0.schema, false);
+
+		//consoleInspect(s1.schema);
+
+		test.ok(true);
+		test.done();
+	},
+};
 
 exports.validate = {
 	'#0-1': tester('#0-1', validateSchema2, { expect: true }, {}),
