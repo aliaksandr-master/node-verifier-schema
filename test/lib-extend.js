7efb07accdabc5128ebe48466fee958ff977201b
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-10 14:54:46 +0300

    add lib/extend tests

---
+++ test/lib-extend.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var extend = require('../lib/extend');
+
+module.exports = {
+	'auto': function (test) {
+		var F = function () {};
+
+		extend(F, Error);
+
+		var f = new F();
+
+		test.ok(f instanceof Error);
+		test.done();
+	},
+
+	'byObjectCreate': function (test) {
+		var F = function () {};
+
+		extend.byObjectCreate(F, Error);
+
+		var f = new F();
+
+		test.ok(f instanceof Error);
+		test.done();
+	},
+
+	'byPrototype': function (test) {
+		var F = function () {};
+
+		extend.byPrototype(F, Error);
+
+		var f = new F();
+
+		test.ok(f instanceof Error);
+		test.done();
+	}
+};
