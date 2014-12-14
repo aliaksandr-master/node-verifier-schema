98af0a7fc0b9e1fe5c73044b274eae8e9d96301b
Aliaksandr <aliaksandr.pasynkau@sport.com>
2014-12-14 22:24:26 +0300

    add validation error support in tester

--- test/lib/tester.js
+++ test/lib/tester.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _ = require('lodash');
 var inspect = require('./inspect');
 
 var tester = function (schema, testCase, objectForValidate) {
@@ -10,7 +11,13 @@ var tester = function (schema, testCase, objectForValidate) {
 				console.log('unexpected error', inspect(err));
 			}
 
-			test.equal(isValid, testCase.expect, 'invalid result ' + inspect(err) + inspect(isValid) + inspect(validationError));
+			if (testCase.validationError != null) {
+				_.each(testCase.validationError, function (v, k) {
+					test.deepEqual(validationError[k], v);
+				});
+			}
+
+			test.strictEqual(isValid, testCase.expect, 'invalid result ' + inspect(err) + inspect(isValid) + inspect(validationError));
 			test.done(err);
 		});
 	};
