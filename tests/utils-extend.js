4a9c5c1f68df308dd3f45d595a70f3820cedb02b
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-21 21:35:34 +0300

    remove unused extend function in utils

--- tests/utils-extend.js
+++ tests/utils-extend.js
@@ -2,37 +2,13 @@
 
 var extend = require('../lib/utils/extend');
 
-module.exports = {
-	'auto': function (test) {
-		var F = function () {};
+exports.instanceof = function (test) {
+	var F = function () {};
 
-		extend(F, Error);
+	extend(F, Error);
 
-		var f = new F();
+	var f = new F();
 
-		test.ok(f instanceof Error);
-		test.done();
-	},
-
-	'byObjectCreate': function (test) {
-		var F = function () {};
-
-		extend.byObjectCreate(F, Error);
-
-		var f = new F();
-
-		test.ok(f instanceof Error);
-		test.done();
-	},
-
-	'byPrototype': function (test) {
-		var F = function () {};
-
-		extend.byPrototype(F, Error);
-
-		var f = new F();
-
-		test.ok(f instanceof Error);
-		test.done();
-	}
+	test.ok(f instanceof Error);
+	test.done();
 };
