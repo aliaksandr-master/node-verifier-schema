77996b705d1c545fd694a560a9b28218def8bff9
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-27 23:48:09 +0300

    add tests for schema yaml parsing

--- tests/parser.js
+++ tests/parser.js
@@ -23,7 +23,7 @@ var schema = new Schema().validate('type object').object(function (r, o) {
 	});
 });
 
-module.exports = {
+exports['simple'] = {
 	'yaml-full': function (test) {
 		test.deepEqual(schema, loader(__dirname + '/for-parser-tests/simple/schema-full.yml'));
 		test.done();
@@ -31,6 +31,38 @@ module.exports = {
 
 	'yaml-short': function (test) {
 		test.deepEqual(schema, loader(__dirname + '/for-parser-tests/simple/schema-short.yml'));
+		test.done();
+	}
+};
+
+var schemaArray = new Schema().optional().validate('type object').array(function (r, o) {
+	r('fio', 'type object').object(function (r, o) {
+		r('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+		r('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+		o('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+	});
+	r('age', ['type number', 'max_value 100', 'min_value 16']);
+	r('family', ['type array', 'min_length 2', {each: ['type object']}]).optional().array(function (r, o) {
+		r('first_name',  ['type string', 'min_length 3', 'max_length 20']);
+		r('last_name',   ['type string', 'min_length 3', 'max_length 20']);
+		o('middle_name', ['type string', 'min_length 3', 'max_length 20']);
+		o('age', ['type number', 'max_value 100', 'min_value 16']);
+	});
+	r('education', ['type array', 'min_length 2', 'not empty', {each: ['type string', 'min_length 3', 'max_length 20']}]).array(function (r, o) {
+		r('name', 'type string');
+		r('type', 'type string');
+		o('classes').array();
+	});
+});
+
+exports['array'] = {
+	'yaml-full': function (test) {
+		test.deepEqual(schemaArray, loader(__dirname + '/for-parser-tests/array/schema-full.yml'));
+		test.done();
+	},
+
+	'yaml-short': function (test) {
+		test.deepEqual(schemaArray, loader(__dirname + '/for-parser-tests/array/schema-short.yml', 'blablabla'));
 		test.done();
 	}
 };
