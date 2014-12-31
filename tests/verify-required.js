4fad5b59fc0005b0dcd8f51014fb744d527affe5
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-12-31 19:57:08 +0300

    add tests for required field testing

---
+++ tests/verify-required.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var _ = require('lodash');
+var tester = require('./_lib/tester');
+var inspect = require('./_lib/inspect');
+var Schema = require('./_lib/schema');
+
+var schemaWithValidation1 = new Schema().object(function (req, opt) {
+	opt('orderby', ['type string', {contains: ['ASC', 'DESC']}]);
+});
+
+exports['base usage'] = {
+	'valid - optional - undefined': tester({
+		schema: new Schema().optional('orderby'),
+		expect: true,
+		value: {}
+	}),
+
+	'valid - optional - has value': tester({
+		schema: new Schema().optional('orderby'),
+		expect: true,
+		value: { orderby: 123 }
+	}),
+
+	'valid - optional validate - undefined': tester({
+		schema: schemaWithValidation1,
+		expect: true,
+		value: {}
+	}),
+
+	'valid - optional validate - has value': tester({
+		schema: schemaWithValidation1,
+		expect: true,
+		value: {
+			orderby: 'ASC'
+		}
+	}),
+
+	'invalid - optional validate - null': tester({
+		schema: schemaWithValidation1,
+		vErr: {
+			rule: 'type',
+			params: 'string',
+			path: [ 'orderby' ]
+		},
+		value: {
+			orderby: null
+		}
+	}),
+
+	'invalid - optional validate - has value': tester({
+		schema: schemaWithValidation1,
+		vErr: {
+			rule: 'contains',
+			params: ['ASC', 'DESC'],
+			path: [ 'orderby' ]
+		},
+		value: { orderby: 'asc' }
+	})
+
+};
